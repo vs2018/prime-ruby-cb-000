55e089782527798a96ab0fdc76cc83a8a89bf633
Vishal Sharma <vsh1819@gmail.com>
2018-06-11 07:07:10 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,25 @@
-# Add  code here!
+def prime?(integer)
+  return true if integer == 2
+  
+  return false if (integer < 2) || (integer > 2 && integer % 2 == 0)
+  
+  new_highest = integer - 1
+  
+  array = (3..new_highest).to_a
+  
+  odd_array = []
+  
+  array.collect do |number|
+    odd_array << number if number.odd?
+  end
+  
+  odd_array.each do |number|
+      if integer % number == 0
+        return false
+      end
+    end
+  
+  return true
+  
+end
+
